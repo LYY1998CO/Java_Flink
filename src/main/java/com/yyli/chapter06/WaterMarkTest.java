e3f02675a89480cb44d75eebf52611a7a7d03e57
LYYAWZY <2715604966@qq.com>
2023-03-14 23:18:40 +0800

    第三次提交

--- src/main/java/com/yyli/chapter06/WaterMarkTest.java
+++ src/main/java/com/yyli/chapter06/WaterMarkTest.java
@@ -24,24 +24,24 @@ public class WaterMarkTest {
                 new Event("Bob", "./pro?id=2", 3800L),
                 new Event("Bob", "./pro??id=3", 4200L)
         )
-                //有序流的watermark生成
-                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forMonotonousTimestamps()
-                .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
-                    @Override
-                    public long extractTimestamp(Event event, long l) {
-                        return event.timestamp;
-                    }
-                })
-        );
-           //无序流的watermark的生成
-//                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(2))
-//                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
-//                            @Override
-//                            public long extractTimestamp(Event event, long l) {
-//                                return event.timestamp;
-//                            }
-//                        })
-//                );
+//                //有序流的watermark生成
+//                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forMonotonousTimestamps()
+//                .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+//                    @Override
+//                    public long extractTimestamp(Event event, long l) {
+//                        return event.timestamp;
+//                    }
+//                })
+//        );
+//           无序流的watermark的生成
+                .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
+                            @Override
+                            public long extractTimestamp(Event event, long l) {
+                                return event.timestamp;
+                            }
+                        })
+                );
         env.execute();
     }
 }
